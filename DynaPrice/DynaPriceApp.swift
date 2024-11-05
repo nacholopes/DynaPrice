aab9c4cc08c09e31bb05c860891206b5bd792452
nacholopes <eduardo@edulopes.com>
2024-11-05 17:23:28 -0300

    Initial Commit

---
+++ DynaPrice/DynaPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynaPriceApp.swift
+//  DynaPrice
+//
+//  Created by Eduardo Lopes on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DynaPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynaPriceTests/DynaPriceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynaPriceTests.swift
+//  DynaPriceTests
+//
+//  Created by Eduardo Lopes on 05/11/24.
+//
+
+import Testing
+@testable import DynaPrice
+
+struct DynaPriceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
